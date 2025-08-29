707f2e76042ecb1ff4674e760ce24b7b125971a7
Noah Vêri Franco <noahvf16@outlook.com>
2025-08-28 21:26:47 -0300

    first commit

---
+++ src/test/java/com/devnoahf/vrumvrumhealth/VrumVrumHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnoahf.vrumvrumhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrumVrumHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
