1897634755b82d345cf9138495e1a71c79b6a89d
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-09-11 16:47:37 -0300

    feat: added Transporte model

--- src/main/java/com/devnoahf/vrumvrumhealth/Model/Transporte.java
+++ src/main/java/com/devnoahf/vrumvrumhealth/Model/Transporte.java
@@ -1,4 +1,26 @@
 package com.devnoahf.vrumvrumhealth.Model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.LocalDateTime;
+
+@Entity
+@Table(name = "transporte")
+@Data
 public class Transporte {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+	private Long id;
+
+	private Long veiculoId;
+
+	// NOTE: it will be needed to format
+	// 'cause in SQL a "TIME" is only "HH:MM:SS", and a date is "YYYY/MM/DD HH:MM:SS"
+	private Date horarioSaida;
+
+	private LocalDateTime dataCriacao;
 }
