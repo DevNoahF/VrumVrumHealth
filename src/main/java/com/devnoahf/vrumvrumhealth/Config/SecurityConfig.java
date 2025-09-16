696a73dbf7a17dcd349da5231338bd33840e8a7d
Noah Vêri Franco <noahvf16@outlook.com>
2025-09-16 19:52:26 -0300

    feat: comentar a criptografia para testes unitarios

--- src/main/java/com/devnoahf/vrumvrumhealth/Config/SecurityConfig.java
+++ src/main/java/com/devnoahf/vrumvrumhealth/Config/SecurityConfig.java
@@ -13,27 +13,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    public Boolean verifyPassword(String rawPassword, String encodedPassword) {
+//        return passwordEncoder().matches(rawPassword, encodedPassword);
+//    }
 
-    public Boolean verifyPassword(String rawPassword, String encodedPassword) {
-        return passwordEncoder().matches(rawPassword, encodedPassword);
-    }
-
-    // Configuração de segurança HTTP para diferentes endpoints baseada nas roles
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // Configura as regras de segurança
-        http  // desabilita CSRF para simplificar (não recomendado para produção sem outras medidas)
-                .csrf(AbstractHttpConfigurer::disable)  // configura as autorizações baseadas em roles
-                .authorizeHttpRequests(auth -> auth // define as regras de autorização
-                        .requestMatchers("/admin/**").hasRole("ADMIN")  // apenas ADMIN pode acessar /admin/**
-                        .requestMatchers("/paciente/**").hasRole("PACIENTE") // apenas PACIENTE pode acessar /paciente/**
-                        .anyRequest().permitAll() // qualquer outra requisição é permitida sem autenticação
-                )
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // permite o formulário de login para todos
-                .logout(LogoutConfigurer::permitAll); // permite logout para todos
-        return http.build(); // constrói a cadeia de filtros de segurança
-    }
+//    // Configuração de segurança HTTP para diferentes endpoints baseada nas roles
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // Configura as regras de segurança
+//        http  // desabilita CSRF para simplificar (não recomendado para produção sem outras medidas)
+//                .csrf(AbstractHttpConfigurer::disable)  // configura as autorizações baseadas em roles
+//                .authorizeHttpRequests(auth -> auth // define as regras de autorização
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")  // apenas ADMIN pode acessar /admin/**
+//                        .requestMatchers("/paciente/**").hasRole("PACIENTE") // apenas PACIENTE pode acessar /paciente/**
+//                        .anyRequest().permitAll() // qualquer outra requisição é permitida sem autenticação
+//                )
+//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // permite o formulário de login para todos
+//                .logout(LogoutConfigurer::permitAll); // permite logout para todos
+//        return http.build(); // constrói a cadeia de filtros de segurança
+//    }
 }
--- src/main/java/com/devnoahf/vrumvrumhealth/Service/AuthService.java
+++ src/main/java/com/devnoahf/vrumvrumhealth/Service/AuthService.java
@@ -12,24 +12,24 @@ import java.util.Optional;
 @Service
 public class AuthService {
 
-    @Autowired
-    private AdmRepository admRepository;  // injeta o repositório de Adm
-
-    @Autowired
-    private PacienteRepository pacienteRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder; // injeta o PasswordEncoder
-
-
-
-
-    // método para autenticar admin
-    public boolean autenticarAdm(String email, String senha){
-        return admRepository.findByEmail(email)
-                .map(adm -> passwordEncoder.matches(senha, adm.getSenhaHash()))
-                .orElse(false);
-    }
+//    @Autowired
+//    private AdmRepository admRepository;  // injeta o repositório de Adm
+//
+//    @Autowired
+//    private PacienteRepository pacienteRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder; // injeta o PasswordEncoder
+//
+//
+//
+//
+//    // método para autenticar admin
+//    public boolean autenticarAdm(String email, String senha){
+//        return admRepository.findByEmail(email)
+//                .map(adm -> passwordEncoder.matches(senha, adm.getSenhaHash()))
+//                .orElse(false);
+//    }
 
 //    // método para autenticar paciente
 //    public boolean autenticarPaciente(String email, String senha){
