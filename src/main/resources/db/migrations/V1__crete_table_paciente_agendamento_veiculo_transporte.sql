d21cac352133a45ad6a554205071615454884ab8
Noah Vêri Franco <noahvf16@outlook.com>
2025-10-08 14:33:37 -0300

    feat: refatoração do banco de dados, utilizando boa pratica com migrations e flyway]

--- src/main/resources/db/migrations/V1__crete_table_paciente_agendamento_veiculo_transporte.sql
+++ src/main/resources/db/migrations/V1__crete_table_paciente_agendamento_veiculo_transporte.sql
@@ -4,7 +4,7 @@ CREATE TABLE paciente (
                           cpf VARCHAR(11) UNIQUE NOT NULL,
                           data_nascimento DATE,
                           email VARCHAR(100) UNIQUE NOT NULL,
-                          senha_hash VARCHAR(255) NOT NULL,
+                          senha VARCHAR(255) NOT NULL,
                           telefone VARCHAR(20),
                           email VARCHAR(100),
                           cep VARCHAR(9),
@@ -13,8 +13,8 @@ CREATE TABLE paciente (
                           numero VARCHAR(10),
                           tipo_atendimento ENUM('EXAME','CONSULTA','TRATAMENTO_CONTINUO') NOT NULL,
                           frequencia ENUM('SEMANAL','QUINZENAL','DIARIO','MENSAL') NULL, -- só preenche se for TRATAMENTO_CONTINUO
-                          data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                           role ENUM('PACIENTE','ADMIN') DEFAULT 'PACIENTE'
 );
 
@@ -23,9 +23,9 @@ CREATE TABLE admin (
                         nome VARCHAR(100) NOT NULL,
                         matricula VARCHAR(20) UNIQUE NOT NULL,
                         email VARCHAR(100) UNIQUE NOT NULL,
-                        senha_hash VARCHAR(255) NOT NULL,
-                        data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                        senha VARCHAR(255) NOT NULL,
+                        create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                         role ENUM('PACIENTE','ADMIN') DEFAULT 'ADMIN'
 );
 CREATE TABLE agendamento (
@@ -35,10 +35,10 @@ CREATE TABLE agendamento (
                              hora_consulta TIME NOT NULL,
                              comprovante_consulta TEXT, -- pode guardar link do storage (S3, Firebase, filesystem)
                              local_atendimento ENUM('UPA1','UPA2','HOSPITAL1','HOSPITAL2') NOT NULL,
-                             estado ENUM('PENDENTE','APROVADO','NEGADO') DEFAULT 'PENDENTE',
+                             status ENUM('PENDENTE','APROVADO','NEGADO') DEFAULT 'PENDENTE',
                              retorno_para_casa BOOLEAN DEFAULT TRUE,
-                             data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                             data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                             create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                             update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                              FOREIGN KEY (paciente_id) REFERENCES paciente(id)
 );
 
@@ -54,7 +54,8 @@ CREATE TABLE transporte (
                             agendamento_id BIGINT NOT NULL,
                             veiculo_id BIGINT,
                             horario_saida TIME NOT NULL,
-                            data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
                             FOREIGN KEY (agendamento_id) REFERENCES agendamento(id),
                             FOREIGN KEY (veiculo_id) REFERENCES veiculo(id),
 );
