e4d70e9c3cebf88528b0bd644b74c4fd1f991cf6
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2025-10-13 14:59:19 -0300

    Started making small tweeks on code with javascript WIP

--- src/pages/javascript/pedido_transporte.js
+++ src/pages/javascript/pedido_transporte.js
@@ -11,3 +11,38 @@ document.getElementById("pedido-transporte").addEventListener("submit",async(e)=
 
 
 });
+
+const motivo=document.getElementById("motivo-value")
+motivo.addEventListener("click", function(){//Função que permite o campo de frequencia aparecer e sumir, css apresenta uma transição simples
+    
+
+    console.log(this.value);
+    if(this.value!="Tratamento Continuo"){
+        frequencia=document.getElementsByClassName("frequencia")[0]
+        frequencia.setAttribute("id","invi")
+    }
+
+
+    if(this.value=="Tratamento Contínuo"){
+        var frequencia=document.getElementById("invi")
+        frequencia.removeAttribute("id")
+    }
+
+})
+
+
+const data_hora=document.getElementById("inputs")
+data_hora.addEventListener("input",function(){//Função que proibe o usuário de colocar uma data no passado
+    const data=document.getElementById("data").value
+    const hora=document.getElementById("hora")
+
+
+    const input_data= new Date(data)
+
+
+    if(data<data_format){
+        console.log("Não é possível colocar essa data")
+    }
+
+ 
+})
