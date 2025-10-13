8e397f9b18aabdc408b38cf3b927224101f51c7c
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2025-10-13 16:19:35 -0300

    Created meia query por resolution 1366px- Notebook

--- src/pages/javascript/pedido_transporte.js
+++ src/pages/javascript/pedido_transporte.js
@@ -30,6 +30,7 @@ motivo.addEventListener("click", function(){//Função que permite o campo de fr
 
 })
 
+var current_time= new Date()
 
 const data_hora=document.getElementById("inputs")
 data_hora.addEventListener("input",function(){//Função que proibe o usuário de colocar uma data no passado
@@ -37,12 +38,21 @@ data_hora.addEventListener("input",function(){//Função que proibe o usuário d
     const hora=document.getElementById("hora")
 
 
-    const input_data= new Date(data)
+    const input_data= new Date(data)//Vários problemas, primeiro que o DIA colocado no site equivale a ao dia anterior, ent dia 13/10=12/10
 
+    console.log(input_data)
 
-    if(data<data_format){
+    if(input_data>current_time){
+        botao=document.getElementById("enviar").disable=true
+        console.log("data válida")
+        console.log(input_data)
+    }
+
+    if(input_data<=current_time || input_data==current_time){//Por algum motivo quando as datas são iguais mesmo com esse operador ele diz que a data é valida!!
+        var botao=document.getElementById("enviar").disable=false//A ideia é que o botão não seja mais clicável, não sei dizer se está funcionando pq nem consigo enviar
         console.log("Não é possível colocar essa data")
     }
 
+//O código em si funciona mas ele tá com esse problema de dar data errada
  
 })
