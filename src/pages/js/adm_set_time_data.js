dbf8402523f90ec731440b2c272616a885073322
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-10-21 21:36:17 -0300

    feat: moved some code around

--- src/pages/js/adm_set_time_data.js
+++ src/pages/js/adm_set_time_data.js
@@ -7,9 +7,18 @@ function formatDate(date) {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
 
+function getOperatorName() {
+    let operatorName = "{operador}";
+
+    // TODO: get operator name from backend 
+
+    return operatorName;
+}
+
 function setOperatorName() {
+    const name = getOperatorName();
     const namePlace = document.getElementsByClassName("dynamic_information")[0];
-    namePlace.innerText = "john doe";
+    namePlace.innerText = name;
 }
 
 function setCurrentDate() {
@@ -17,13 +26,13 @@ function setCurrentDate() {
     const datePlace = document.getElementsByClassName("dynamic_information")[1];
     const formatedTime = formatTime(date);
     const formatedDate = formatDate(date);
-			
+
     datePlace.innerText = `Horas atuais: ${formatedTime}, ${formatedDate}`;
 }
 
-function setContent() {
+function setTimedContent() {
     setOperatorName();
     setCurrentDate();
 }
 
-setContent();
+setTimedContent();
