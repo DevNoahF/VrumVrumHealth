1e55f951fb549a7b5345b7347bafdb3447086260
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2025-11-11 00:31:46 -0300

    status-solicitacao is working, if I have time i will refactor it

--- src/pages/js/status-solicitacao.js
+++ src/pages/js/status-solicitacao.js
@@ -1,8 +1,15 @@
-authToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJub3ZvLmFkbWluQHZydW0uY29tIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpYXQiOjE3NjI4MjExODUsImV4cCI6MTc2MjkwNzU4NX0.5iQBCtjDUWbA2LdM74E6OJ_6s7WtV6p_F7Eerz94IWQYoQL_hnWYkKVbZ4WDIO9jL5Wu8DlQ1CZuqgH11tPiHg"
+authToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJub3ZvLmFkbWluQHZydW0uY29tIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpYXQiOjE3NjI4MzA0OTEsImV4cCI6MTc2MjkxNjg5MX0.fCiCDtwoW0bN35Yxs8vQCVM-WOVLnjGc0yDsYHmY_oned3q5QRMxrYDLuhaQopHDvxfGLWFn4OtSQvBj5n1deQ"
+var nome=document.getElementById("paciente")
+var endereco= document.getElementById("endereco")
 var unidade=document.getElementById("unidade")
 var estado = document.getElementById("status")
+var acompanhante = document.getElementById("acompanhante")
+var retorno=document.getElementById("retorno")
+var motivo=document.getElementById("tipo-tratamento")
+var frequencia=document.getElementById("frequencia")
+var dia= document.getElementById("data")
 
-
+//Função pega dados de agendamento
 async function getAgendamento(id){//Precisa de id agendamento
     const get_agendamento = await fetch("http://localhost:8080/agendamento/"+ id, {
       method: "GET",
@@ -11,29 +18,93 @@ async function getAgendamento(id){//Precisa de id agendamento
          'Authorization': `Bearer ${authToken}`
        },
     })
+
     const dados= await get_agendamento.json()
+    const pacienteDado=getPaciente(dados.pacienteId)
 
     const dado_agendamento = {
-      id: dados.id,
       dataConsulta: dados.dataConsulta,
       horaConsulta: dados.horaConsulta,
       comprovante: dados.comprovante,
       localAtendimentoEnum: dados.localAtendimentoEnum,
       statusEnum: dados.statusEnum,
       retornoCasa: dados.retornoCasa,
-      tratamentoContinuo: dados.tratamentoContinuo,
       tipoAtendimentoEnum: dados.tipoAtendimentoEnum,
-      frequencia: dados.frequencia,
+      frequencia:dados.frequencia,
       acompanhante: dados.acompanhante,
       pacienteId: dados.pacienteId
     };
-   
 
-    unidade.textContent=unidade.textContent+String(dado_agendamento.localAtendimentoEnum)
-    estado.textContent=estado.textContent+dado_agendamento.statusEnum
+
+   
+    nome.textContent+=(await pacienteDado).nome
+    endereco.textContent+=(await pacienteDado).rua+(" ")+(await pacienteDado).numero+(" ")+(await pacienteDado).bairro
+    unidade.textContent+=dado_agendamento.localAtendimentoEnum
+    estado.textContent+=dado_agendamento.statusEnum
+    motivo.textContent+=dado_agendamento.tipoAtendimentoEnum
+    frequencia.textContent+=dado_agendamento.frequencia
+    dia.textContent+=dado_agendamento.dataConsulta+"-"+dado_agendamento.horaConsulta
+    acompanhante.textContent+=changeValue(dado_agendamento.acompanhante)
+    retorno.textContent+=changeValue(dado_agendamento.retornoCasa)
+
+      const divAnexo = document.getElementById("visualizar-anexo");
+  if (dado_agendamento.comprovante) {
+    divAnexo.onclick = () => {
+      // abre em nova aba, por exemplo
+      window.open(dado_agendamento.comprovante, "_blank");
+    };
+  }
+
     return dado_agendamento
 }
 
+//Pega dados de Paciente
+async function getPaciente(id) {
+  const get_paciente = await fetch("http://localhost:8080/paciente/"+id,{
+    method: "GET",
+    mode:'cors',
+    headers: {"Content-Type": "application/json",
+      'Authorization': `Bearer ${authToken}`
+    },
+  })
+  const dados= await get_paciente.json()
+  const dadosPaciente={
+    nome:dados.nome,
+    rua:dados.rua,
+    bairro:dados.bairro,
+    numero:dados.numero
+  }
+  return dadosPaciente
+}
+async function cancelarAgendamento(id){
+  document.getElementById("cancelar").addEventListener("click", async function(){
+    const delPaciente= await fetch("http://localhost:8080/agendamento/"+id,{
+      method:"DELETE",
+      mode:"cors",
+      headers: {"Content-Type": "application/json",
+      'Authorization': `Bearer ${authToken}`
+    },
+    })
+  })
+}
 
-console.log(getAgendamento(1))
+//Mudar os valores de true e false para Sim e não
+function changeValue(valor) {
+
+  if(valor==true){
+    return "Sim"
+  }
+  if(valor==false){
+    return "Não"
+  }
+  return "Erro, dado não encontrado";
+  
+}
+
+
+
+
+console.log(getAgendamento(2))
+
+cancelarAgendamento(1)
 
