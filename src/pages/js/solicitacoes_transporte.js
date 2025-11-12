3be865b57df523499a46ba78d9a1e3ee566f4d5d
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2025-11-12 00:04:45 -0300

    buttons of aproval em reproval are working

--- src/pages/js/solicitacoes_transporte.js
+++ src/pages/js/solicitacoes_transporte.js
@@ -1,4 +1,4 @@
-authToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJub3ZvLmFkbWluQHZydW0uY29tIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpYXQiOjE3NjI5MTM4ODMsImV4cCI6MTc2MzAwMDI4M30._HdU4dLVhN62Fy7j3PPT91I6wz4VnpPRkoy7D_vBBcjNqflWTq-7qBKAmWUWFB1x1ErCE8EA8u4rMIqNX-SbUw"
+authToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJub3ZvLmFkbWluQHZydW0uY29tIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpYXQiOjE3NjI5MTY0ODYsImV4cCI6MTc2MzAwMjg4Nn0.RtQb8W_cOfrD21ZyBIZ6Xv7toNAy84sooTS9uzEiAYre2YOw74svwsIl2KApiAV1-gVVRJ7xjI7QV-OXsVt4VQ"
 var idAgendamento=document.getElementById("id")
 var nome=document.getElementById("nome")
 var data=document.getElementById("data")
@@ -22,13 +22,11 @@ async function agendamentoData(id){
        
 
     })
-    const dadosPaciente=await pacienteData(1)
-    console.log(dadosPaciente)
     const dados= await agendamento.json()
     const dado_agendamento = {
       dataConsulta: dados.dataConsulta,
       horaConsulta: dados.horaConsulta,
-      comprovante: dados.comprovante,
+      comprovante:dados.comprovante,
       localAtendimentoEnum: dados.localAtendimentoEnum,
       statusEnum: dados.statusEnum,
       retornoCasa: dados.retornoCasa,
@@ -40,22 +38,28 @@ async function agendamentoData(id){
       id:dados.id,
       estado:dados.statusEnum
     };
-  console.log(dado_agendamento)
 
-    idAgendamento.textContent=("#")+dado_agendamento.id
-    nome.textContent+=(await dadosPaciente).nome
-    data.textContent+=dado_agendamento.dataConsulta+("-")+dado_agendamento.horaConsulta
-    nascimento.textContent+=(await dadosPaciente).dataNasc
-    cpf.textContent+=(await dadosPaciente).cpf
-    endereco.textContent+=(await dadosPaciente).rua+(" ")+(await dadosPaciente).numero+(" ")+(await dadosPaciente).bairro
-    motivo.textContent+=dado_agendamento.tipoAtendimentoEnum
-    unidade.textContent+=dado_agendamento.localAtendimentoEnum
-    frequencia.textContent+=dado_agendamento.frequencia
-    acompanhante.textContent+=changeValue(dado_agendamento.acompanhante)
-    transporte_volta.textContent+=changeValue(dado_agendamento.retornoCasa)
+
+
 
     return dado_agendamento
 
+}
+
+async function mostrarMensagem(id){
+  const dadosPaciente=await pacienteData(id)
+  const dadosAgendamento=await agendamentoData(id)
+    idAgendamento.textContent=("#")+dadosAgendamento.id
+    nome.textContent+=(await dadosPaciente).nome
+    data.textContent+=dadosAgendamento.dataConsulta+("-")+dadosAgendamento.horaConsulta
+    nascimento.textContent+=(await dadosPaciente).dataNasc
+    cpf.textContent+=(await dadosPaciente).cpf
+    endereco.textContent+=(await dadosPaciente).rua+(" ")+(await dadosPaciente).numero+(" ")+(await dadosPaciente).bairro
+    motivo.textContent+=dadosAgendamento.tipoAtendimentoEnum
+    unidade.textContent+=dadosAgendamento.localAtendimentoEnum
+    frequencia.textContent+=dadosAgendamento.frequencia
+    acompanhante.textContent+=changeValue(dadosAgendamento.acompanhante)
+    transporte_volta.textContent+=changeValue(dadosAgendamento.retornoCasa)
 }
 
 async function pacienteData(id) {
@@ -91,8 +95,18 @@ function changeValue(valor) {
   
 }
 
-agendamentoData(1)
-aprovar(1)
+async function verAnexo(id) {
+  comprovante=await agendamentoData(id)
+  const divAnexo = document.getElementById("anexo");
+  if (comprovante.comprovante) {
+    divAnexo.onclick = () => {
+      // abre em nova aba, por exemplo
+      window.open(comprovante.comprovante, "_blank");
+    };
+    console.log(comprovante.comprovante)
+  }
+  
+}
 
 async function aprovar(id){
   agendamentoAprovado = await agendamentoData(id)
@@ -110,3 +124,26 @@ async function aprovar(id){
     console.log(agendamentoAprovado)
   })
 }
+
+async function recusar(id){
+  agendamentoAprovado = await agendamentoData(id)
+  document.getElementById("recusar").addEventListener("click",async function(){
+    agendamentoAprovado.statusEnum="NEGADO"
+    const aprovar = await fetch("http://localhost:8080/agendamento/"+id,{
+      method:"PUT",
+      mode:"cors",
+      headers:{"Content-Type": "application/json",
+         'Authorization': `Bearer ${authToken}`
+       },
+       body:JSON.stringify(agendamentoAprovado)
+
+    })
+    console.log(agendamentoAprovado)
+  })
+}
+
+//Isso aqui precisa de uma função para colocar o id
+mostrarMensagem(1)
+aprovar(1)
+recusar(1)
+verAnexo(1)
